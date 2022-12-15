108a6c911c35a20e8af786723e8401d7da5effdf
Lizachann <liza.chan@student.cadt.edu.kh>
2022-12-15 13:59:30 +0700

    Change q to l

--- crudquiz1.cpp
+++ crudquiz1.cpp
@@ -74,7 +74,7 @@ void deleteQuestion(List *ls, int index){
 void createQuestions(){
  List* ls;
     ls = createEmptyList();
-    AddtoList(ls, 1 ,"question :"," answer1: "," answer2: "," answer3: ");
+    AddtoList(ls, 1 ,"luestion :"," answer1: "," answer2: "," answer3: ");
     AddtoList(ls, 2 ,"question :"," answer1: "," answer2: "," answer3: ");
     AddtoList(ls, 3 ,"question :"," answer1: "," answer2: "," answer3: ");
     AddtoList(ls, 4 ,"question :"," answer1: "," answer2: "," answer3: ");
