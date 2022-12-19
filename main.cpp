c6d142369d465d47dd31d221eb834649bfa78078
RUiNe4 <sunchhay@gmail.com>
2022-12-19 10:56:00 +0700

    store

--- main.cpp
+++ main.cpp
@@ -1,36 +1,46 @@
-#include "dataStructure.h"
+#include "login.h"
 using namespace std;
 
-// int input;
-int main(){
-  List *mainList;
-  mainList = createEmptyList();
-  createQuestions(mainList);
+void home(List *mainList){
   mainMenu();
-  cout<<"Enter your choice >>>>> ";
-  cin>>input;
+  cout<<"Enter your choice >>>>> "; cin>>input;
   switch(input){
     case 1:
     //Login admin
     adminOpt(mainList);
     break;                                                                                                        
     case 2:
-
-    cout<<"WIP"<<endl;
+    signUp(&fname, &lname, &email, &password, &rpassword);
+    if(checkExistEmailInFile(email)==0){
+        cout<<"User already exist"<<endl;
+    }else if(checkExistEmailInFile(email)==1){
+        cout<<"You have successfully registered"<<endl;
+        insert(mainList,fname,lname,email,password);
+    }
+    home(mainList);
     //Sign up 
     break;
     case 3:
     //Login student
-    cout<<"WIP"<<endl;
+    Login();
+    // cout<<"WIP"<<endl;
     break;
     case 4:
-    cout<<"WIP"<<endl;
+    cout<<"You have exit the program"<<endl;
+    // cout<<"WIP"<<endl;
     //Exit
     break;
     default:
     cout<<"Invalid option"<<endl;
     break;
   }
+}
 
+// int input;
+int main(){
+  List *mainList;
+  mainList = createEmptyList();
+  createQuestions(mainList);
+  home(mainList);
   return 0;
 }
