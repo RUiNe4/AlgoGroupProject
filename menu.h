344d75f8f04fabe83432991b29dbba824ee45a45
Liza123J <johnliza77@gmail.com>
2022-12-26 14:49:18 +0700

    add loading function

--- menu.h
+++ menu.h
@@ -1,19 +1,60 @@
 #include "header.h"
 
 inline void exit(string msg){
-  system("cls");
+  system("clear");
   cout<<msg<<endl;
-  _sleep(500);
+  //_sleep(500);
+  sleep(500);
 }
 
 inline void invalidOpt(){
-  system("cls");
+  //system("cls");
+  system("clear");
   cout<<"Invalid option - Press any key to continue";
   // _sleep(500);
 }
+void loadingBar()
+{
+  
+    cout<<"\t\t\t\t\t\t****************************"<<endl;
+    cout<<"\t\t\t\t\t\t** WELCOME TO QUIZ SYSTEM **"<<endl;
+    cout<<"\t\t\t\t\t\t****************************"<<endl<<endl;
+	// 0 - black background,
+	// A - Green Foreground
+	system("color 0D");
+
+	// Initialize char for printing
+	// loading bar
+	char a = 177, b = 219;
+
+	cout<<"\n\n\n\n";
+	cout<<"\n\n\n\n\t\t\t\t\t"
+		"Loading...\n\n";
+	cout<<"\t\t\t\t\t";
+
+	// Print initial loading bar
+	for (int i = 0; i < 26; i++){
+		cout<<"%c", a;
+  }
+	// Set the cursor again starting
+	// point of loading bar
+	cout<<"\r";
+	cout<<"\t\t\t\t\t";
+
+	// Print loading bar progress
+	for (int i = 0; i < 26; i++) {
+		cout<<("%c", b);
+
+		// Sleep for 1 second
+  
+    //_sleep(1000);
+		sleep(1000);
+	}
+}
 
 void mainMenu(){
-  system("cls");
+  //system("cls");
+  system("clear");
   cout<<"Who are you?"<<endl;
   cout<<"1 - Admin"<<endl;
   cout<<"2 - Take Test"<<endl;
@@ -21,14 +62,15 @@ void mainMenu(){
 }
 
 inline void testTakerMenu(){
-  system("cls");
+  //system("cls");
+  system("clear");
   cout<<"1 - Sign up"<<endl;
   cout<<"2 - Already have an account? ==> Take test"<<endl;
   cout<<"3 - Back to menu"<<endl;
 }
-
-inline void adminMenu(){
-  system("cls");
+ void adminMenu(){
+  //system("cls");
+  system("clear");
   cout<<"1. Add questions"<<endl;
   cout<<"2. Display Current question"<<endl;
   cout<<"3. Remove questions"<<endl;
@@ -39,7 +81,8 @@ inline void adminMenu(){
 }
 
 inline void studentMenu(){
-  system("cls");
+  //system("cls");
+  system("clear");
   cout<<"1. Take test"<<endl;
   cout<<"2. Display Result"<<endl;
   cout<<"3. Back to menu"<<endl;
